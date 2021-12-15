ce06e62ef2e07014542ea48923f41410b220b559
Tiziana <titianamannucci@gmail.com>
2021-12-15 12:16:57 +0100

    error test

--- src/main/java/main/Hello.java
+++ src/main/java/main/Hello.java
@@ -1,9 +1,9 @@
-package main;
+package main.java.main;
 
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         String s;
-        boiiiii
+        //error prone
     }
 }
